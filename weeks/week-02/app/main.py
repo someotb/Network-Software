be13a74c6852c49b6a6594e1959f493878445164
someotb <llyryk934@gmail.com>
2026-02-26 11:23:30 +0700

    feat: add 2nd lab

--- weeks/week-02/app/main.py
+++ weeks/week-02/app/main.py
@@ -1,6 +1,54 @@
+import uuid
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 
 app = FastAPI()
 
-# В этом месте вам необходимо расширить CRUD и добавить валидацию
+
+class Book(BaseModel):
+    id: str | None = None
+    name: str
+    date: str
+
+
+books: dict[str, Book] = {}
+
+
+@app.get("/bookings")
+async def get_books():
+    return books
+
+
+@app.get("/bookings/{id}")
+async def get_book(id: str):
+    book = books.get(id)
+    if not book:
+        raise HTTPException(status_code=404)
+    return book
+
+
+@app.post("/bookings", status_code=201)
+async def post_book(book: Book):
+    book.id = str(uuid.uuid4())
+    books[book.id] = book
+    return book
+
+
+@app.put("/bookings/{id}")
+async def put_book(id: str, updated_book: Book):
+    if id not in books:
+        raise HTTPException(status_code=404)
+
+    updated_book.id = id
+    books[id] = updated_book
+    return updated_book
+
+
+@app.delete("/bookings/{id}")
+async def delete_book(id: str):
+    book = books.get(id)
+    if not book:
+        raise HTTPException(status_code=404)
+
+    books.pop(id)
