3b32788fed58acfa2e8520f30518a099be2bf140
someotb <llyryk934@gmail.com>
2026-02-26 10:50:27 +0700

    feat: add 1st lab

--- weeks/week-01/app/main.py
+++ weeks/week-01/app/main.py
@@ -1,6 +1,35 @@
+import uuid
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 
 app = FastAPI()
 
-# В этом месте вам необходимо реализовать REST API согласно варианту
+
+class Book(BaseModel):
+    id: str | None = None
+    name: str
+    date: str
+
+
+books: dict[str, Book] = {}
+
+
+@app.get("/bookings")
+async def get_books():
+    return books
+
+
+@app.get("/bookings/{id}")
+async def get_book(id: str):
+    book = books.get(id)
+    if not book:
+        raise HTTPException(status_code=404)
+    return book
+
+
+@app.post("/bookings", status_code=201)
+async def create_book(book: Book):
+    book.id = str(uuid.uuid4())
+    books[book.id] = book
+    return book
