7c7e63423359d5efff24d79944ba5dfc687bc60a
someotb <llyryk934@gmail.com>
2026-05-06 19:24:04 +0700

    chore: rm comments

--- weeks/week-07/starter/service.py
+++ weeks/week-07/starter/service.py
@@ -9,15 +9,12 @@ tickets_db = []
 
 class TicketsServiceServicer(service_pb2_grpc.TicketsServiceServicer):
     def CreateTicket(self, request, context):
-        # Создаём новый тикет
         new_ticket = service_pb2.Ticket(
             id=str(len(tickets_db) + 1),
             text=request.text,
             status=request.status
         )
-        # Добавляем в базу
         tickets_db.append(new_ticket)
-        # Возвращаем Response
         return service_pb2.CreateTicketResponse(
             id=new_ticket.id,
             text=new_ticket.text,
@@ -25,7 +22,6 @@ class TicketsServiceServicer(service_pb2_grpc.TicketsServiceServicer):
         )
 
     def GetTickets(self, request, context):
-        # Возвращаем все тикеты
         return service_pb2.GetTicketsResponse(tickets=tickets_db)
 
 def serve():
